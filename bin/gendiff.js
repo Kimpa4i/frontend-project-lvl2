1cdca579fdcaffa24c1e45acd9ff7d2d965cf243
andrew kim <mr.kimpa4i@gmail.com>
2022-07-26 00:17:35 +0300

    add argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,12 @@
 const { program } = require('commander');
 
 program
+  .option('-f, --format <type>', 'output format')
   .name('gendiff')
+  .argument('<filepath1>,<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0');
+
 
 
 program.parse();
