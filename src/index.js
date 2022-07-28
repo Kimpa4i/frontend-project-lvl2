a7052935029ced7f6c3532357c53b1663bc5e1d4
andrew kim <mr.kimpa4i@gmail.com>
2022-07-28 14:41:10 +0300

    add getFixturePath

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
-// import { path } from 'node:path';
 import { cwd } from 'node:process';
 import path from 'node:path';
 import { readFileSync } from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-
-// const a = path.resolve('/foo/bar', '/tmp/file/');
-// console.log(a);
-
-// console.log(`Current directory: ${cwd()}`);
 
 const diff = (filepath1,filepath2) => {
-const absolutePath = path.resolve(filepath1);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const relativePath = getFixturePath(filepath1);
+console.log(readFileSync(path.resolve(relativePath),'utf8'));
+console.log(readFileSync(path.resolve(filepath2),'utf8'));
 
-console.log(readFileSync(absolutePath,'utf8'));
-// console.log(absolutePath);
+
+
 }
 export default diff;
-// const read = readFileSync('/Users/andrew/Desktop/frontend-project-lvl2/__fixtures__/file1.json','utf8');
-// const read2 = readFileSync('__fixtures__/file1.json','utf8');
 
-// // // console.log(read);
-// console.log(read2);
 
-// const filePuth = 'file1.json';
-// const absolutePath = path.resolve(filePuth);
-// console.log(readFileSync(absolutePath,'utf8'));
-// console.log(readFileSync(newPath));
-// __fixtures__/file1.json
