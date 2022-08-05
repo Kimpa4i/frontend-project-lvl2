84c12b0a9e7e31e1c60dc54f65c930d2f726c2e2
andrew kim <mr.kimpa4i@gmail.com>
2022-08-05 13:12:41 +0300

    add jest

---
+++ __tests__/test.js
@@ -0,0 +1,5 @@
+import diff from "../src/index.js";
+
+// test('p', () => {
+//     expect(2+2).toBe(4);
+//   });
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-// eslint-disable-next-line import/extensions
 import diff from '../src/index.js';
 
 const program = new Command();
