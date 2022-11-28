5c95427ace46b8f125c6153849e41462e997aee3
andrew kim <mr.kimpa4i@gmail.com>
2022-11-28 21:41:32 +0500

    fix

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -7,6 +7,8 @@ const formatter = (data, format) => {
       return stylish(data);
     case 'plain':
       return plain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       throw new Error(`format ${formatter} is not supported`);
   }
